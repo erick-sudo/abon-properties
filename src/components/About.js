d015e8165a6e471b7bc5984edbfc86dc34271b1a
erick-sudo <erickochieng766@gmail.com>
2023-03-14 01:25:40 +0300

    Leadership

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,13 @@ import mission from "../assets/about/mission.png"
 import vision from "../assets/about/vision.png"
 import purpose from "../assets/about/purpose.png"
 
+import placeholder1 from "../assets/about/placeholder1.jpg"
+import placeholder2 from "../assets/about/placeholder2.jpg"
+import placeholder3 from "../assets/about/placeholder3.jpg"
+import placeholder4 from "../assets/about/placeholder4.jpg"
+
+import { ImOffice, ImWhatsapp } from 'react-icons/im'
+
 import { GrServices, GrBlog, GrContactInfo } from "react-icons/gr"
 
 function About() {
@@ -28,6 +35,33 @@ function About() {
             </div>
 
             <Ribbon />
+
+            <div className="bg-sky-300 p-3">
+                <h1 className="text-xl font-bold pl-4">Our Leadership</h1>
+                <div className="grid md:grid-cols-2">
+                    <Person fullname="Sherley Powers" contact={<><ImWhatsapp /> <span className="pl-3">+1433-675-533</span></>} position="Manager" description="Omnis incidunt ullam rem et eligendi aspernatur beatae delectus non culpa aut quisquam quod eveniet." avatar={placeholder1} />
+                    <Person fullname="Duane Pollich" contact={<><ImWhatsapp /> <span className="pl-3">+344-776-342</span></>} position="Finance" description="Omnis incidunt ullam rem et eligendi aspernatur beatae delectus non culpa aut quisquam quod eveniet." avatar={placeholder2} />
+                    <Person fullname="Samella Lindgren" contact={<><ImWhatsapp /> <span className="pl-3">+999-234-945</span></>} position="Accounts" description="Omnis incidunt ullam rem et eligendi aspernatur beatae delectus non culpa aut quisquam quod eveniet." avatar={placeholder3} />
+                    <Person fullname="Kristopher Zemlak" contact={<><ImWhatsapp /> <span className="pl-3">+999-234-945</span></>} position="Auditor" description="Omnis incidunt ullam rem et eligendi aspernatur beatae delectus non culpa aut quisquam quod eveniet." avatar={placeholder4} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Person({fullname, position, avatar, description, contact}) {
+    return (
+        <div className="bg-sky-200 mr-3 mb-3 p-4 rounded-lg max-w-lg shadow-md shadow-black hover:bg-white 
+          ">
+            <div className="flex items-center">
+                <img src={avatar} className="h-32 rounded-full" alt={fullname} />
+                <div className="grow border-b border-black pb-2 ml-4">
+                    <div className="font-bold text-xl text-blue-900">{fullname}</div>
+                    <div className="font-bold">{position}</div>
+                </div>
+            </div>
+            <p className="">{description}</p>
+            <div className="flex justify-end items-center text-slate-700 font-bold">{contact}</div>
         </div>
     )
 }
