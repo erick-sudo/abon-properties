d26a386b6d28189ef588bbc34f94b695b0312e9b
erick-sudo <erickochieng766@gmail.com>
2023-03-18 01:12:15 +0300

    Properties

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { CgArrowLongRightR } from "react-icons/cg"
 import { TiStarburst } from "react-icons/ti"
-import { MdOutlineArrowBackIosNew, MdOutlineArrowForwardIos } from "react-icons/md"
+import { MdOutlineArrowBackIosNew, MdOutlineArrowForwardIos, MdOutlinePreview} from "react-icons/md"
+
+import b1 from "../assets/properties/p1/b1.png"
+
+import { SlLike } from "react-icons/sl"
+import { GrView } from "react-icons/gr"
+import { ImStarFull, ImStarHalf, ImStarEmpty } from "react-icons/im"
 
 function Properties() {
 
@@ -13,13 +19,13 @@ function Properties() {
                 <h1 className="text-6xl my-3">Your trusted partners in property</h1>
                 
                 <h3 className="mt-7 px-3 font-normal text-sm text-center">We provide innovative property solutions for our clients that add tangible value across property sectors and services</h3>
-                <div className="grow flex justify-center items-center text-sky-400"><span className="text-2xl flex items-center"><MdOutlineArrowBackIosNew /></span><TiStarburst className="text-8xl animate-bounce transition duration-1000" /><span className="text-2xl flex items-center"><MdOutlineArrowForwardIos /></span></div>
+                <div className="grow flex justify-center items-center text-gray-400"><span className="text-2xl flex items-center"><MdOutlineArrowBackIosNew /></span><TiStarburst className="text-8xl animate-bounce transition duration-1000" /><span className="text-2xl flex items-center"><MdOutlineArrowForwardIos /></span></div>
             </div>
-            <div className="grid sm:grid-cols-2 md:grid-cols-3">
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
                 {
                     properties.map((property, index) => {
                         return (
-                            <Property key={index} title={property} />
+                            <Property key={index} title={property} star_rating={Math.floor(Math.random()*100)%10} views={Math.floor(Math.random()*100000)} likes={Math.floor(Math.random()*1000000)} />
                         )
                     })
                 }
@@ -28,14 +34,38 @@ function Properties() {
     )
 }
 
-function Property({title}) {
+function Property({title, star_rating, likes, views}) {
     return (
-        <div className="relative bg-sky-400 mt-3 mr-3 ml-3 shadow-black shadow-md">
+        <div className="relative bg-gray-200 mt-3 mr-3 ml-3 shadow-black shadow-md">
             <div className="">
-                <img className="" src="https://cdn.pixabay.com/photo/2016/09/14/20/50/london-1670432_960_720.png" alt="..." />
+                <img className="" src={b1} alt="..." />
             </div>
-            <h3 className="font-bold p-2">{title}</h3>
-            <button className="absolute left-2 bottom-20 text-2xl text-red-700 bg-black w-max px-6 hover:text-white hover:scale-110 "><CgArrowLongRightR /></button>
+            <div className="flex flex-row-reverse my-1">
+                <button className="w-1/2 mr-3 text-2xl text-red-700 bg-black px-8 py-1 hover:text-white hover:scale-110 "><CgArrowLongRightR /></button>
+            </div>
+            <h3 className="font-bold p-2 absolute top-2">{title}</h3>
+            <Status star_rating={star_rating} likes={likes} views={views} />
+        </div>
+    )
+}
+
+function Status({star_rating, likes, views}) {
+    return (
+        <div className="">
+            <div className="grid grid-cols-2">
+                <span className="flex items-center text-sm">{<SlLike className="text-xl mx-2 text-sky-700" />}{likes} Likes</span>
+                <span className="flex items-center text-sm">{<MdOutlinePreview className="text-xl mx-2 text-sky-700" />}{views} Views</span>
+            </div>
+            <div className="font-bold">
+                <div className="text-center">Rating {star_rating}</div>
+                <div className="flex justify-center pb-3 text-sky-700">
+                    {
+                        new Array(10).fill(<ImStarEmpty />).map((star, index) => {
+                            return index === star_rating ? <ImStarHalf key={index} /> : index < star_rating ? <ImStarFull key={index} /> : <ImStarEmpty key={index} />
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
